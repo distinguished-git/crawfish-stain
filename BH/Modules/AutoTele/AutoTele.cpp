c6a6fb2bd312a2fc9ff6fdc2fd1c91ef6db349b3
distinguished-git <distinguished.web@gmail.com>
2019-01-11 16:12:07 -0500

    .

--- BH/Modules/AutoTele/AutoTele.cpp
+++ BH/Modules/AutoTele/AutoTele.cpp
@@ -211,16 +211,13 @@ void AutoTele::OnKey(bool up, BYTE key, LPARAM lParam, bool* block) {
 	else if (key == WPKey) 
 	{
 		POINT Vectorz[5];
-		Vectorz1[1] = FindPresetLocation(UNIT_OBJECT, 237, Areas[0])
+		Vectorz1[1] = FindPresetLocation(UNIT_OBJECT, 237, GetPlayerArea())
 		RunTo(Vectorz1[1].x);
 //		ManageTele(vVector[GetPlayerArea()*4+2]);
 	}
 	else if (key == PrevKey) 
 	{
-		POINT Vectorz[5];
-		Vectorz1[1] = FindPresetLocation(UNIT_OBJECT, 237, Areas[1])
-		RunTo(Vectorz1[1].y);
-//		ManageTele(vVector[GetPlayerArea()*4+3]);
+		ManageTele(vVector[GetPlayerArea()*4+3]);
 	}
 }
 
